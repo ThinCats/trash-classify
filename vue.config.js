da6f5ee8dd41229b458e5770ebaa7534941ae5ce
thincats <thincats@163.com>
2019-07-12 23:48:01 +0800

    chore(css): add global scss file support

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  chainWebpack: config => {
+    const ofs = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    const cssRules = config.module.rule('css')
+    const postRules = config.module.rule('postcss')
+
+    const addSassResourcesLoader = (rules, type) => {
+      rules
+        .oneOf(type)
+        .use('sass-resoureces-loader')
+        .loader('sass-resources-loader')
+        .options({
+          // resources: [path.resolve(__dirname, './src/styles/general.scss')]
+        })
+    }
+    ofs.forEach(type => {
+      addSassResourcesLoader(cssRules, type)
+      addSassResourcesLoader(postRules, type)
+    })
+    return config
+  },
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        data: `@import "~@/styles/general.scss";`
+      }
+    }
+  }
+}
