dc52caa4c7e107fe2477e42a17941b805c62e69f
thincats <thincats@163.com>
2019-07-12 13:35:12 +0800

    feat(graphql-server): add graphql-server support

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   client: {
+    // service: {
+    //   // localSchemaFile: './src/schemas/main.graphql'
+    // },
     service: {
-      localSchemaFile: './src/schemas/main.graphql'
+      name: 'graphql-faker',
+      url: 'http://localhost:9002/graphql',
+      // optional disable SSL validation check
+      skipSSLValidation: true
     },
     includes: ['src/**/*.{js,jsx,ts,tsx,vue,gql}']
   }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 import './plugins/element.js'
-import { createProvider } from './vue-apollo'
+//@ts-ignore
+import { createProvider, VueApollo } from './vue-apollo'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+declare module 'vue/types/vue' {
+  interface Vue {
+    apolloProvider: VueApollo
+    $message: CallableFunction
+  }
+}
 
 new Vue({
   router,
   store,
+  //@ts-ignore
   apolloProvider: createProvider(),
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
